b924ccd1d7ad9e2b2fd99d9388b708aa630228ce
Your Name <willw@example.com>
2013-07-28 19:31:39 +0000

    modifications to index.html

--- web.js
+++ web.js
@@ -3,10 +3,11 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+    var buffer = new Buffer(fs.readFileSync("index.html"));
+    response.send(buffer.toString());
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-  console.log("Listening on " + port);
+    console.log("Listening on " + port);
 });
